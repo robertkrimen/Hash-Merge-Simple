8c0d11d88333974440b546f61a37cf1d304f904a
rob <rob@bc20d3b6-2a10-dd11-90db-000c6e5764c0>
2008-04-22 05:40:44 +0000

    ''

---
+++ lib/Hash/Merge/Simple.pm
@@ -0,0 +1,107 @@
+package Hash::Merge::Simple;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Hash::Merge::Simple - The great new Hash::Merge::Simple!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Hash::Merge::Simple;
+
+    my $foo = Hash::Merge::Simple->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Robert Krimen, C<< <rkrimen at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-hash-merge-simple at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Hash-Merge-Simple>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Hash::Merge::Simple
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Hash-Merge-Simple>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Hash-Merge-Simple>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Hash-Merge-Simple>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Hash-Merge-Simple>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 Robert Krimen, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Hash::Merge::Simple
